a40a5c5ccc001552a5d3038ea4312f80f0a046bf
georgev93 <georgeavogt93@gmail.com>
2025-12-11 21:59:26 +0530

    Benchmarking pt 1 and pt 2

--- benches/my_bench.rs
+++ benches/my_bench.rs
@@ -1,9 +1,21 @@
-use aoc_2025_01::solve;
+use aoc_2025_01::{solve, solve_pt1, solve_pt2};
 use criterion::{Criterion, criterion_group, criterion_main};
 
 fn bench_solve(c: &mut Criterion) {
-    c.bench_function("solve input.txt", |b| b.iter(|| solve("data/input.txt")));
+    c.bench_function("Combined", |b| b.iter(|| solve("data/input.txt")));
 }
 
-criterion_group!(benches, bench_solve);
+fn bench_pt1(c: &mut Criterion) {
+    c.bench_function("Part 1", |b| b.iter(|| solve_pt1("data/input.txt")));
+}
+
+fn bench_pt2(c: &mut Criterion) {
+    c.bench_function("Part 2", |b| b.iter(|| solve_pt2("data/input.txt")));
+}
+
+criterion_group! {
+name = benches;
+config= Criterion::default();
+targets= bench_solve, bench_pt1, bench_pt2
+}
 criterion_main!(benches);
--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use crate::dial::Dial;
 mod rotations;
 use crate::rotations::Rotations;
 
-mod file_parser;
+pub mod file_parser;
 use crate::file_parser::parse_file;
 
 pub fn solve(input_file: &str) -> (u32, u32) {
@@ -12,6 +12,20 @@ pub fn solve(input_file: &str) -> (u32, u32) {
     let mut dial = Dial::new(rotations.get_iter());
     dial.roll_through();
     (dial.get_zero_instances(), dial.get_zero_clicks())
+}
+
+pub fn solve_pt1(input_file: &str) -> u32 {
+    let rotations = Rotations::new(parse_file(input_file));
+    let mut dial = Dial::new(rotations.get_iter());
+    dial.roll_through();
+    dial.get_zero_instances()
+}
+
+pub fn solve_pt2(input_file: &str) -> u32 {
+    let rotations = Rotations::new(parse_file(input_file));
+    let mut dial = Dial::new(rotations.get_iter());
+    dial.roll_through();
+    dial.get_zero_clicks()
 }
 
 #[cfg(test)]
